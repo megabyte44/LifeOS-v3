af41171657737bf31d2aa7b246a830a9bdc48f81
LifeOS Refactor <refactor@lifeos.dev>
2026-06-26 12:46:41 +0530

    fixes

--- backend/src/main/java/com/lifos/backend/service/InsightGeneratorService.java
+++ backend/src/main/java/com/lifos/backend/service/InsightGeneratorService.java
@@ -213,4 +213,3 @@ public class InsightGeneratorService {
         return aiConfigurationResolver.resolveProviderApiKey(provider, config.getApiKeys());
     }
 }
-                                                                                                                                                                                 
